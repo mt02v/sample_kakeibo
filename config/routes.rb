8eb8bbb1987d6d30d0cd712f0bea01cc39ac88ad
mt02v <141305346+mt02v@users.noreply.github.com>
2023-12-24 14:58:22 +0000

    [Add] balance_confirm/show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   post "income_values/new(/:name)" => "income_values#new"
   post "fixedcost_values/new(/:name)" => "fixedcost_values#new"
   post "variablecost_values/new(/:name)" => "variablecost_values#new"
+
+  get "balance_confirm" => "balance_confirm#top"
+  post "baalance_confirm/show(/:name)" => "balance_confirm#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :incomes
   resources :fixedcosts
