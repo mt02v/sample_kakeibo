5c5cc00469be78064e5663be51fb13a1f5a3ae74
mt02v <141305346+mt02v@users.noreply.github.com>
2023-12-25 13:55:14 +0000

    [Finish] シンプルな家計簿アプリを作る(3)

--- app/controllers/balance_confirm_controller.rb
+++ app/controllers/balance_confirm_controller.rb
@@ -40,10 +40,12 @@ class BalanceConfirmController < ApplicationController
     #年度収入配列を作成
     i=0
     total = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
+  gon.data_incomes = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil] #グラフ用データ
     year_monts.each do |year_months|
       income_values = IncomeValue.where(year_month: year_month)
       if income_values.present?
         total[i] = cal_income_total(income_values)
+    gon.data_fixedcosts[i] = total[i] #グラフ用データ
       end
       i += 1
     end
@@ -52,22 +54,26 @@ class BalanceConfirmController < ApplicationController
     #年度の固定費配列を作成
     i=0
     total = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
+  gon.data_variablecosts = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil] #グラフ用データ
     year_months.each do |year_month|
       fixedcost_values = FixedcostValue.where(year_month: year_month)
       if fixedcost_values.present?
          total[i] = col_fixedcost_total(fixedcost_values)
+    gon.data_variablecosts[i] = total[i] #グラフ用データ
       end
       i += 1
     end
-    @fixedcost_value_totals = total
+    @variablecost_value_totals = total
 
     #年度の変動費配列を作成
     i=0
     total = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
+  gon.data_results = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil] #グラフ用データ
     year_months.each do |year_month|
       variablecost_values = VariablecostValue.where(year_month: year_month)
       if variablecost_values.present?
         total[i] = cal_variablecost_total(variablecost_values)
+    gon.data_variablecosts[i] = total[i] #グラフ用データ
       end
       i += 1
     end
@@ -75,9 +81,11 @@ class BalanceConfirmController < ApplicationController
 
       #年度の収支結果を計算
       @balance_differences = [nil,nil,nil,nil,nil,nil,nil,nil]
+  gon.data_results = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil] #グラフ用データ
       0.upto(11) do |i|
         if @income_valnce_totals[i].present?
           @balannce_differences[i] = @income_valance_totals[i] - (@fixedcost_value_totals[i] + @variablecost_value_totals[i])
+      gon.data_results[i] = @balance_differences[i] #グラフ用データ
         end
       end
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+//
+//= require Chart.min
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
